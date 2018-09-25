a6e2518fafad4638ea49744b96e92531a7834e72
papa <papathiame11@gmail.com>
2018-09-25 12:07:33 +0000

    first commit

---
+++ src/Blog/BlogBundle/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Blog\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('email', EmailType::class)->add('password', PasswordType::class)->add('confirm_password', PasswordType::class)->add('sexe')->add('telephone', IntegerType::class)->add('datenaissance');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\BlogBundle\Entity\User',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'blog_blogbundle_user';
+    }
+}
